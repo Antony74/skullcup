272d65207535c7f3a1bb841894018fab2e373732
Antony74 <akb@akb.me.uk>
2024-01-18 22:07:09 +0000

    Start working with meshes

--- skullcup.py
+++ skullcup.py
@@ -1,3 +1,13 @@
 # usage:
 # docker run -it -v .:/working pymesh/pymesh python3 /working/skullcup.py
-print("Hello world")
+
+import pymesh
+
+cup = pymesh.load_mesh('/working/Coffee_Cup.A.1.stl')
+skull = pymesh.load_mesh('/working/Scull_geant_fix02.stl')
+
+# skullcup = pymesh.convex_hull(cup)
+# skullcup = pymesh.boolean(cup, skull, 'union', 'cork')
+skullcup = pymesh.merge_meshes([cup, skull])
+
+pymesh.save_mesh('/working/skullcup.stl', skullcup)
