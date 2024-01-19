920666f42eff0d106cbff3606fee78078356d10a
Antony74 <akb@akb.me.uk>
2024-01-19 17:25:08 +0000

    Figured out how to do rotation

--- skullcup.py
+++ skullcup.py
@@ -2,12 +2,17 @@
 # docker run -it -v .:/working pymesh/pymesh python3 /working/skullcup.py
 
 import pymesh
+from scipy.spatial.transform import Rotation
 
 cup = pymesh.load_mesh('/working/Coffee_Cup.A.1.stl')
 skull = pymesh.load_mesh('/working/Scull_geant_fix02.stl')
 
+rotation = Rotation.from_euler('x', 45, degrees=True).as_matrix()
+
+newCup = pymesh.form_mesh([rotation.dot(v) for v in cup.vertices], cup.faces)
+
 # skullcup = pymesh.convex_hull(cup)
 # skullcup = pymesh.boolean(cup, skull, 'union', 'cork')
-skullcup = pymesh.merge_meshes([cup, skull])
+skullcup = pymesh.merge_meshes([newCup, skull])
 
 pymesh.save_mesh('/working/skullcup.stl', skullcup)
