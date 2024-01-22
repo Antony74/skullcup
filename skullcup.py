dda07dfbb36896ccd52a7d2e484a49a84385f552
Antony74 <akb@akb.me.uk>
2024-01-22 21:01:31 +0000

    Start removing handle

--- skullcup.py
+++ skullcup.py
@@ -9,7 +9,7 @@ def transformMesh(mesh, fn):
     return pymesh.form_mesh([fn(v) for v in mesh.vertices], mesh.faces)
 
 
-scale = 0.75
+scale = 0.6
 
 initialCup = pymesh.load_mesh('/working/Coffee_Cup.A.1.stl')
 initialSkull = pymesh.load_mesh('/working/Scull_geant_fix02.stl')
@@ -19,19 +19,24 @@ cupRotationMatrix = Rotation.from_euler('y', 180, degrees=True).as_matrix()
 skullRotationMatrix = Rotation.from_euler(
     'xy', [-90, 90], degrees=True).as_matrix()
 
-# cupBox = pymesh.generate_box_mesh(initialCup.bbox[0], initialCup.bbox[1])
-# skullBox = pymesh.generate_box_mesh(initialSkull.bbox[0], initialSkull.bbox[1])
-
 cup = transformMesh(initialCup, lambda v: cupRotationMatrix.dot(v))
 
-skull = transformMesh(initialSkull, lambda v: scale * skullRotationMatrix.dot(v))
+skull = transformMesh(initialSkull, lambda v: scale *
+                      skullRotationMatrix.dot(v))
 
 yAdjustment = cup.bbox[0][1] - skull.bbox[0][1]
 
-skull = transformMesh(skull, lambda v: v + [0, yAdjustment, 0])
+skull = transformMesh(skull, lambda v: v + [0, yAdjustment, 10])
+
+handleBox = pymesh.generate_box_mesh([-20, -20, -50], [-70, 50, 50])
+
+handle = pymesh.boolean(cup, handleBox, 'difference', 'cork')
+
+handle = transformMesh(handle, lambda v: v + [-50, 0, 0])
 
 # skullcup = pymesh.convex_hull(cup)
 # skullcup = pymesh.boolean(cup, skull, 'union', 'cork')
-skullcup = pymesh.merge_meshes([cup, skull])
+skullcup = pymesh.merge_meshes([cup,  # skull, handleBox, handle
+                                handle])
 
 pymesh.save_mesh('/working/skullcup.stl', skullcup)
