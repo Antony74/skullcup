af7b725623741335719da333ae7964e717824554
Antony74 <akb@akb.me.uk>
2024-01-24 22:03:47 +0000

    refactor.  MeshObj

--- skullcup.py
+++ skullcup.py
@@ -2,7 +2,6 @@
 # docker run -it -v .:/working pymesh/pymesh python3 /working/skullcup.py
 
 import pymesh
-from pymesh import convex_hull
 from scipy.spatial.transform import Rotation
 
 
@@ -10,18 +9,31 @@ def transformMesh(mesh, fn):
     return pymesh.form_mesh([fn(v) for v in mesh.vertices], mesh.faces)
 
 
-def add(mesh1, mesh2):
-    return pymesh.boolean(mesh1, mesh2, 'union', 'cork')
+class MeshObj(object):
 
+    def __init__(self, mesh):
+        self._mesh = mesh
 
-def subtract(mesh1, mesh2):
-    return pymesh.boolean(mesh1, mesh2, 'difference', 'cork')
+    def mesh(self):
+        return self._mesh
+
+    def __add__(self, other):
+        return MeshObj(pymesh.boolean(self._mesh, other._mesh, 'union', 'cork'))
+
+    def __sub__(self, other):
+        return MeshObj(pymesh.boolean(self._mesh, other._mesh, 'difference', 'cork'))
+    
+    def convex_hull(self):
+        return MeshObj(pymesh.convex_hull(self.mesh()))
 
 
 scale = 0.6
 
-cup = pymesh.load_mesh('/working/Coffee_Cup.A.1.stl')
-skull = pymesh.load_mesh('/working/Scull_geant_fix02.stl')
+# TO DO URL
+cupMesh = pymesh.load_mesh('/working/Coffee_Cup.A.1.stl')
+
+# TO DO URL
+skullMesh = pymesh.load_mesh('/working/Scull_geant_fix02.stl')
 
 cupRotationMatrix = Rotation.from_euler('y', 180, degrees=True).as_matrix()
 handleBoxRotationMatrix = Rotation.from_euler('z', 5, degrees=True).as_matrix()
@@ -34,22 +46,24 @@ skullRotationMatrix = Rotation.from_euler(
 # cut through to the cup's interior will be fine.
 handle = pymesh.generate_box_mesh([-70, -20, -50], [-25, 50, 50])
 
-handle = transformMesh(handle, lambda v: handleBoxRotationMatrix.dot(v))
+handle = MeshObj(transformMesh(handle, lambda v: handleBoxRotationMatrix.dot(v)))
 
-cup = transformMesh(cup, lambda v: cupRotationMatrix.dot(v))
+cupMesh = transformMesh(cupMesh, lambda v: cupRotationMatrix.dot(v))
 
-cupWithoutHandle = subtract(cup, handle)
+cup = MeshObj(cupMesh)
 
-skull = transformMesh(skull, lambda v: scale *
+cupWithoutHandle = (cup - handle).mesh()
+
+skullMesh = transformMesh(skullMesh, lambda v: scale *
                       skullRotationMatrix.dot(v))
 
 cupCenterX = (cupWithoutHandle.bbox[0][0] + cupWithoutHandle.bbox[1][0]) / 2
-skullCenterX = (skull.bbox[0][0] + skull.bbox[1][0]) / 2
+skullCenterX = (skullMesh.bbox[0][0] + skullMesh.bbox[1][0]) / 2
 xAdjustment = cupCenterX - skullCenterX
-yAdjustment = cup.bbox[0][1] - skull.bbox[0][1]
+yAdjustment = cupMesh.bbox[0][1] - skullMesh.bbox[0][1]
 
-skull = transformMesh(skull, lambda v: v + [xAdjustment, yAdjustment, 20])
+skull = MeshObj(transformMesh(skullMesh, lambda v: v + [xAdjustment, yAdjustment, 20]))
 
-skullcup = add(subtract(skull, convex_hull(subtract(cup, handle))), cup)
+skullcup = skull - (cup - handle).convex_hull() + cup
 
-pymesh.save_mesh('/working/skullcup.stl', skullcup)
+pymesh.save_mesh('/working/skullcup.stl', skullcup.mesh())
