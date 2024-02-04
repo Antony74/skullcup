7ade81ac176bf59a3013420a52518da6061dbea2
Antony74 <akb@akb.me.uk>
2024-02-04 12:44:25 +0000

    Integrate the mesh 'fixes' which run easily

--- skullcup.py
+++ skullcup.py
@@ -1,25 +1,12 @@
 # usage:
 # docker run -it -v .:/working pymesh/pymesh python3 /working/skullcup.py
 
+from pathlib import Path
 import pymesh
 from scipy.spatial.transform import Rotation
 import sys
+from numpy.linalg import norm
 
-sys.path.append('/usr/local/bin')
-
-from fix_mesh import fix_mesh  # noqa: E402
-
-# Load mesh files
-#
-# Mesh files to be place in same directory as this script
-
-# https://cults3d.com/en/3d-model/home/coffee-cup
-print('Loading cup')
-cupMesh = fix_mesh(pymesh.load_mesh('/working/Coffee_Cup.A.1.stl'))
-
-# https://cults3d.com/en/3d-model/various/to-make-or-not-to-make
-print('Loading skull')
-skullMesh = fix_mesh(pymesh.load_mesh('/working/Scull_geant_fix02.stl'))
 
 # Helper class
 
@@ -48,6 +35,84 @@ def convex_hull(meshObj):
 def transformMesh(mesh, fn):
     return pymesh.form_mesh([fn(v) for v in mesh.vertices], mesh.faces)
 
+
+def fix_mesh(mesh, detail="normal"):
+    bbox_min, bbox_max = mesh.bbox
+    diag_len = norm(bbox_max - bbox_min)
+    if detail == "normal":
+        target_len = diag_len * 5e-3
+    elif detail == "high":
+        target_len = diag_len * 2.5e-3
+    elif detail == "low":
+        target_len = diag_len * 1e-2
+    # print("Target resolution: {} mm".format(target_len))
+
+    # count = 0
+    print('remove_degenerated_triangles')
+    mesh, __ = pymesh.remove_degenerated_triangles(mesh, 100)
+    print('split_long_edges')
+    mesh, __ = pymesh.split_long_edges(mesh, target_len)
+    # num_vertices = mesh.num_vertices
+    # while True:
+    #     mesh, __ = pymesh.collapse_short_edges(mesh, 1e-6)
+    #     mesh, __ = pymesh.collapse_short_edges(mesh, target_len,
+    #                                            preserve_feature=True)
+    #     mesh, __ = pymesh.remove_obtuse_triangles(mesh, 150.0, 100)
+    #     if mesh.num_vertices == num_vertices:
+    #         break
+
+    #     num_vertices = mesh.num_vertices
+    #     print("#v: {}".format(num_vertices))
+    #     count += 1
+    #     if count > 10:
+    #         break
+
+    print('resolve_self_intersection')
+    mesh = pymesh.resolve_self_intersection(mesh)
+    print('remove_duplicated_faces')
+    mesh, __ = pymesh.remove_duplicated_faces(mesh)
+    # mesh = pymesh.compute_outer_hull(mesh)
+    # mesh, __ = pymesh.remove_duplicated_faces(mesh)
+    print('remove_obtuse_triangles')
+    mesh, __ = pymesh.remove_obtuse_triangles(mesh, 179.0, 5)
+    print('remove_isolated_vertices')
+    mesh, __ = pymesh.remove_isolated_vertices(mesh)
+
+    return mesh
+
+
+# Load mesh files
+#
+# Mesh files to be place in same directory as this script
+
+filenameCup = '/working/cup_fixed.stl'
+filenameSkull = '/working/skull_fixed.stl'
+
+print('Loading cup')
+
+if (Path(filenameCup).exists()):
+    cupMesh = pymesh.load_mesh(filenameCup)
+else:
+    # https://cults3d.com/en/3d-model/home/coffee-cup
+    cupMesh = pymesh.load_mesh('/working/Coffee_Cup.A.1.stl')
+    print('Fixing cup')
+    cupMesh = fix_mesh(cupMesh)
+    pymesh.save_mesh(filenameCup, cupMesh)
+
+print('Loading skull')
+
+if (Path(filenameSkull).exists()):
+    skullMesh = pymesh.load_mesh(filenameSkull)
+else:
+    # https://cults3d.com/en/3d-model/various/to-make-or-not-to-make
+    skullMesh = pymesh.load_mesh('/working/Scull_geant_fix02.stl')
+    print('Fixing skull')
+    skullMesh = fix_mesh(skullMesh)
+    pymesh.save_mesh(filenameSkull, skullMesh)
+
+exit
+
+
 # Handle
 #
 # We use a convex hull to 'fill' the cup and thus exclude the skull from the cup's
@@ -58,6 +123,7 @@ def transformMesh(mesh, fn):
 # be certain we're not missing any of the handle.  Any amount of cup that doesn't
 # cut through to the cup's interior will be fine.
 
+print('Calculating handle')
 
 handleBoxRotationMatrix = Rotation.from_euler('z', 5, degrees=True).as_matrix()
 
@@ -68,9 +134,13 @@ handle = MeshObj(transformMesh(
 
 # Lip - a box containing the lip of the cup
 
+print('Calculating lip')
+
 lip = MeshObj(pymesh.generate_box_mesh([-70, 35, -70], [70, 50, 70]))
 
 # Cup
+
+print('Orientating cup')
 
 cupRotationMatrix = Rotation.from_euler('y', 180, degrees=True).as_matrix()
 
@@ -81,6 +151,8 @@ cup = MeshObj(cupMesh)
 cupWithoutHandle = (cup - handle).mesh()
 
 # Skull
+
+print('Orientating skull')
 
 # first scale skull appropriately with respect to cup, and align rotationally
 
@@ -104,8 +176,9 @@ skull = MeshObj(transformMesh(skullMesh, lambda v: v +
 
 # Skullcup
 
-skullcup = cup
-# skullcup = skull - convex_hull(cup - handle) + cup
+print('Combining to create skullcup')
+
+skullcup = skull - convex_hull(cup - handle) + cup
 # skullcup = MeshObj(pymesh.merge_meshes([skull.mesh(), cup.mesh(), lip.mesh()]))
 
 pymesh.save_mesh('/working/skullcup.stl', skullcup.mesh())
