ab2f37393bc3a686ca01b136939d6ba15a08c0f4
Antony74 <akb@akb.me.uk>
2024-03-09 21:10:53 +0000

    mesh_info and biggest_piece

--- helpers.py
+++ helpers.py
@@ -1,18 +1,23 @@
 from pathlib import Path
+from functools import reduce
+import numpy as np
 import pymesh
 from fix_mesh.fix_mesh import fix_mesh
 
 
-def load_fixed_mesh(originalFilename, fixedFilename):
+def load_fixed_mesh(originalFilename, fixedFilename, pinMesh=None):
     if (Path(fixedFilename).exists()):
         print('Loading ' + fixedFilename)
         mesh = pymesh.load_mesh(fixedFilename)
     else:
         print('Loading ' + originalFilename)
         mesh = pymesh.load_mesh(originalFilename)
+        mesh_info(mesh)
         print('Fixing ' + originalFilename)
         mesh = fix_mesh(mesh)
         pymesh.save_mesh(fixedFilename, mesh)
+
+    mesh_info(mesh)
     return mesh
 
 
@@ -45,5 +50,17 @@ def convex_hull(meshObj):
     return MeshObj(name, pymesh.convex_hull(meshObj.mesh()))
 
 
-def transformMesh(mesh, fn):
-    return pymesh.form_mesh([fn(v) for v in mesh.vertices], mesh.faces)
+def mesh_info(mesh):
+    print('vertices ' + str(len(mesh.vertices)) +
+          ', faces ' + str(len(mesh.faces)))
+
+
+def biggest_piece(mesh):
+    def reducer(acc, piece): return acc if len(
+        acc.faces) > len(piece.faces) else piece
+
+    emptyMesh = pymesh.form_mesh(np.empty(0), np.empty([0, 0]))
+
+    sep = pymesh.separate_mesh(mesh)
+
+    return reduce(reducer, sep, emptyMesh)
