b9c3fb241c57f2b67950e0c1d1c2fda539b06819
Antony74 <akb@akb.me.uk>
2024-06-02 09:55:43 +0100

    create masks for the sides of the prism

--- src/prism.py
+++ src/prism.py
@@ -4,14 +4,37 @@ import pymesh
 from common.AffineMatrix import AffineMatrix
 from common.helpers import save_mesh_verbose
 
-unitBox = pymesh.generate_box_mesh([-0.5, -0.5, -0.5], [0.5, 0.5, 0.5])
+rotationMatrix = AffineMatrix().rotateX(0.25 * math.pi)
+translationMatrix = AffineMatrix().translate(0.5, 0, 0)
 
-unitBox = AffineMatrix().rotateX(0.25 * math.pi).dot(unitBox)
+# 'bottom' denotes the origin (0,0,0) end of the prism 'top' the other end
+# 'left' and 'right' are the two sides off the prism - their respective assignment 
+# is arbitrary, so there's a 50% chance I've assigned them correctly ;-)
+
+unitBox = pymesh.generate_box_mesh([-0.5, -0.5, -0.5], [0.5, 0.5, 0.5])
+maskTopRight = pymesh.generate_box_mesh([0, 0.5, -0.5], [0.5, 1.5, 0.5])
+maskBottomRight = pymesh.generate_box_mesh([-0.5, 0.5, -0.5], [0, 1.5, 0.5])
+maskTopLeft = pymesh.generate_box_mesh([0, -0.5, -1.5], [0.5, 0.5, -0.5])
+maskBottomLeft = pymesh.generate_box_mesh([-0.5, -0.5, -1.5], [0, 0.5, -0.5])
+
+unitBox = rotationMatrix.dot(unitBox)
+maskTopRight = rotationMatrix.dot(maskTopRight)
+maskBottomRight = rotationMatrix.dot(maskBottomRight)
+maskTopLeft = rotationMatrix.dot(maskTopLeft)
+maskBottomLeft = rotationMatrix.dot(maskBottomLeft)
 
 remove = pymesh.generate_box_mesh([-2, -2, -2], [2, 0, 2])
 
 difference = pymesh.boolean(unitBox, remove, 'difference')
 
-out = AffineMatrix().translate(0.5, 0, 0).dot(difference)
+prism = translationMatrix.dot(difference)
+maskTopRight = translationMatrix.dot(maskTopRight)
+maskBottomRight = translationMatrix.dot(maskBottomRight)
+maskTopLeft = translationMatrix.dot(maskTopLeft)
+maskBottomLeft = translationMatrix.dot(maskBottomLeft)
 
-save_mesh_verbose('working/prism.stl', out)
+save_mesh_verbose('working/prism.stl', prism)
+save_mesh_verbose('working/maskTopRight.stl', maskTopRight)
+save_mesh_verbose('working/maskBottomRight.stl', maskBottomRight)
+save_mesh_verbose('working/maskTopLeft.stl', maskTopLeft)
+save_mesh_verbose('working/maskBottomLeft.stl', maskBottomLeft)
