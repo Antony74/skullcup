77fc0ca6f332f4a960bb46ee5c6692659de78608
Antony74 <akb@akb.me.uk>
2024-06-27 22:33:40 +0100

    Fix validation errors in prism and cylinder

--- src/prism.py
+++ src/prism.py
@@ -13,14 +13,20 @@ unitBox = rotationMatrix.dot(unitBox)
 
 remove = pymesh.generate_box_mesh([-2, -2, -2], [2, 0, 2])
 
-difference = pymesh.boolean(unitBox, remove, 'difference')
+difference = pymesh.boolean(unitBox, remove, 'difference', engine='cgal')
 
 prism = translationMatrix.dot(difference)
 
 coneHeight = prism.bbox[1][1]
 coneBase = -0.1
 coneRadius = linearMap(coneBase, 0, coneHeight, coneHeight, 0)
-cone = pymesh.generate_cylinder([0, coneBase, 0], [0, coneHeight, 0], coneRadius, 0, num_segments=32)
+cone = pymesh.generate_cylinder([0, coneBase, 0], [0, coneHeight, 0], coneRadius, 0.1 * coneRadius, num_segments=32)
+
+print('remove_duplicated_vertices')
+prism, __ = pymesh.remove_duplicated_vertices(prism)
+
+print('remove_degenerated_triangles')
+prism, __ = pymesh.remove_degenerated_triangles(prism, 100)
 
 save_mesh_verbose('working/prism.stl', prism)
 save_mesh_verbose('working/cone.stl', cone)
