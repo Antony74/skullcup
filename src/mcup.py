5f1ca5e0658c1835dd900852cecbccad545476f1
Antony74 <akb@akb.me.uk>
2024-07-06 21:22:56 +0100

    Restore original fix_mesh script

--- src/mcup.py
+++ src/mcup.py
@@ -2,6 +2,7 @@ import math
 import pymesh
 from common.AffineMatrix import AffineMatrix
 from common.helpers import save_mesh_verbose
+from fix_mesh.fix_mesh import fix_mesh
 
 m = pymesh.load_mesh('working/mWithSurface.stl')
 cup = pymesh.load_mesh('working/cupCenteredIgnoringHandle.stl')
@@ -16,5 +17,6 @@ scale = 100 / (out.bbox[1][1] - out.bbox[0][1])
 
 out = AffineMatrix().scale(scale).dot(out)
 
-save_mesh_verbose('working/mcupUnfixed.stl', out)
+out = fix_mesh(out)
 
+save_mesh_verbose('mcup.stl', out)
