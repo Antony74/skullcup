efe9acb774a8b31cf3c31a4d41d194c850bbef8d
Antony74 <akb@akb.me.uk>
2024-08-11 16:08:40 +0100

    refactor memoize functions

--- src/memoize.ts
+++ src/memoize.ts
@@ -1,21 +1,32 @@
-export const memoize = <ParamsType extends Array<any>, ReturnType>(
+const memoizeExposed = <ParamsType extends Array<any>, ReturnType>(
     fn: (...params: ParamsType) => ReturnType,
 ) => {
     const cache: Record<string, ReturnType> = {};
-    return (...params: ParamsType) => {
-        const key = JSON.stringify(params);
-        return cache[key] ? cache[key] : (cache[key] = fn(...params));
+
+    return {
+        fn: (...params: ParamsType) => {
+            const key = JSON.stringify(params);
+            return cache[key] ? cache[key] : (cache[key] = fn(...params));
+        },
+        getCache: (): Record<string, ReturnType> => {
+            return cache;
+        },
     };
 };
+
+export const memoize = <ParamsType extends Array<any>, ReturnType>(
+    fn: (...params: ParamsType) => ReturnType,
+) => memoizeExposed(fn).fn;
 
 export const memoizeBounded = <ParamsType extends Array<any>, ReturnType>(
     fn: (...params: ParamsType) => ReturnType,
     maxSize: number,
 ) => {
-    const cache: Record<string, ReturnType> = {};
+    const base = memoizeExposed(fn);
+
     return (...params: ParamsType) => {
-        const key = JSON.stringify(params);
-        const result = cache[key] ? cache[key] : (cache[key] = fn(...params));
+        const result = base.fn(...params);
+        const cache = base.getCache();
 
         const keys = Object.keys(cache);
         if (keys.length > maxSize) {
